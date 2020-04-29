5394613bfa025e2a8a22f00dc33b6caa9f54e086
huhong12345 <hhuxidian@gmail.com>
2020-04-29 19:43:48 +0800

    huhong

--- Graph_Create.m
+++ Graph_Create.m
@@ -1,58 +1,41 @@
-
 % -------------------------------------------------------------------------
-%%%程序功能：建立图（节点、边）的元胞，计算t时刻各节点的产值V和弹性值R
+%%%程序功能：建立图（节点、边）的元胞
 %%%程序输入：
 %节点层数Chain_layer_Num
-%各层节点最大节点个数Max_node_num_of_one_layer  （1*Chain_layer_Num维矩阵）
 %%%程序输出：
 %Graph:记录网络每层的节点编号
 %Arc:记录网络边的连接
-%R:记录各节点的R值
-%
-%CoopNum:基本合作数，即每个人可以投出的合作数
 % -------------------------------------------------------------------------
-clc;clear;
-Chain_layer_Num=8;    %节点层数
-Arc_layer_Num=Chain_layer_Num-1;   %边层数
-
-
+function [Graph,Arc] = Graph_create(Chain_layer_Num)
 %图节点
+Graph = cell(1,Chain_layer_Num);
 node_sum=0;    %节点总数
 node_num=zeros(1,Chain_layer_Num);   %各层节点个数
-Max_node_num_of_one_layer=10*ones(1,Chain_layer_Num); %每一层最大节点个数
-Min_node_num_of_one_layer=4*ones(1,Chain_layer_Num); %每一层最小节点个数
+Max_node_num=10*ones(1,Chain_layer_Num); %每一层最大节点个数
+Min_node_num_=4*ones(1,Chain_layer_Num); %每一层最小节点个数
 
-Graph = cell(1,Chain_layer_Num);
 for i=1:Chain_layer_Num
-    node_num(i)=randi([Min_node_num_of_one_layer(i),Max_node_num_of_one_layer(i)],1);    %第i层节点个数从10中随机抽取随机整数
+    node_num(i)=randi([Min_node_num(i),Max_node_num(i)]);    %第i层真实节点个数
     Graph{i}=((node_sum+1):(node_sum+node_num(i)));
     node_sum=node_sum+node_num(i);
 end
 
 %边连接
+Arc_layer_Num=Chain_layer_Num-1;   %边层数
 Arc = cell(1,Arc_layer_Num);
 for i=1:Arc_layer_Num
-    Max_arc_num_of_one_layer=length(Graph{i})*length(Graph{i+1});   %第i层最大边数（设置为全连接时）
-    Min_arc_num_of_one_layer=ceil(Max_arc_num_of_one_layer/3);     %第i层最小边数（取为全连接的一半，向上取整）
-    Arc_num(i)=randi([Min_arc_num_of_one_layer,Max_arc_num_of_one_layer],1);                  %第i层的真实边数
+    Max_arc_num=length(Graph{i})*length(Graph{i+1});   %第i层最大边数（设置为全连接时）
+    Min_arc_num=ceil(Max_arc_num/3);     %第i层最小边数（取为全连接的1/3，向上取整）
+    Arc_num(i)=randi([Min_arc_num,Max_arc_num],1);                  %第i层的真实边数
     Connect_matrix=zeros(Arc_num(i),2);  %第i层的连接矩阵(边数*2矩阵)
     for j=1:Arc_num(i)
-        Connect_matrix(j,1)=randi([Graph{i}(1),Graph{i}(1)+length(Graph{i})-1],1); %Node_choose_i_layer
+        Connect_matrix(j,1)=randi([Graph{i}(1),Graph{i}(1)+length(Graph{i})-1]); %Node_choose_i_layer
         Connect_matrix(j,2)=randi([Graph{i+1}(1),Graph{i+1}(1)+length(Graph{i+1})-1],1);  %Node_choose_i+1_layer
     end
     Arc{i}=Connect_matrix;
+end    
 end
-        
-%R值
-R = cell(1,3);
-R{1} = rand(3,1);%第一层的R值
-R{2} = rand(2,1);
-R{3} = rand(2,1);
 
-%改变概率矩阵P
-P = cell(1,3); 
-P{1} = rand(3,1);%第一层的P值
-P{2} = rand(2,1);
-P{3} = rand(2,1);
+
 
 
---
+++ R_Calc.m
@@ -0,0 +1,29 @@
+% -------------------------------------------------------------------------
+% 计算节点t+1时刻的R值。
+% 该值取决于上一时刻的R值和上一时刻的上下游产量。
+% 即对任一节点i，R(t+1)=(1-alpha)R(t)+alpha*g(V(t),Vu(t)，Vd(t))
+% 其中g函数设定为g()~N((a*V+b*Vu+c*Vd),0.1)     即取一个以上下游产量线性组合为均值的高斯分布，a,b,c为可调参数
+% 输入参数:连接关系、本时刻各节点弹性值，产值  
+% 输出参数:下一时刻弹性值
+% -------------------------------------------------------------------------
+function R_new = R_calc(Arc,R，V)
+    R_new = R;
+    g = R;
+    alpha = 0.1;  %弱连接系数
+    a = 1/3; b = 1/3; c = 1/3; %产值线性组合参数
+    sigma = 0.1;  %高斯分布方差
+    gama = 10;    %调节g大小的参数
+    for i = 1:length(R)
+        for j = 1:length(R{i})
+            if (i == 1)
+                temp = a*V{i}(j)+c*sum(V{i+1})
+            elseif (i == length(R))
+                temp = a*V{i}(j)+b*sum(V{i-1})
+            else
+                temp = a*V{i}(j)+b*sum(V{i-1})+c*sum(V{i+1})    
+            end   
+            g{i}(j) = gama*normrnd(temp, sigma);
+            R_new{i}(j) = (1-alpha)*R{i}(j)+alpha*g{i}(j)
+        end
+    end   
+end
