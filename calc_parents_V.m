afd39b6327d6d9e65e4ee653df0c58eab0fdf5fe
DWX97 <DWX97@users.noreply.github.com>
2020-05-01 20:54:22 +0800

    change length to size(,1)
    change length to size(,1)

--- calc_parents_V.m
+++ calc_parents_V.m
@@ -1,11 +1,11 @@
-%%è¿”å›žGraph{i}(j)çˆ¶èŠ‚ç‚¹çš„æ€»äº§å€¼
+%%·µ»ØGraph{i}(j)¸¸½ÚµãµÄ×Ü²úÖµ
 function sum_parents_V = calc_parents_V(Graph,Arc,V_list,i,j)
     if i==1
         sum_parents_V = 0;
     else
         parents = [];
         sum_parents_V = 0;
-        for u = 1:length(Arc{i-1})
+        for u = 1:size(Arc{i-1}, 1)
             if Arc{i-1}(u,2)==Graph{i}(j)
                 sum_parents_V=sum_parents_V+V_list(Arc{i-1}(u,1));
             end
