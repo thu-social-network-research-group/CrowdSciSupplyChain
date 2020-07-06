220e2f60c352b89788df242c694f4570ddff5c60
Melody618 <Melody618@sjtu.edu.cn>
2020-07-06 17:16:47 +0800

    删点删边，重新编号

--- RemoveNode.m
+++ RemoveNode.m
@@ -1,7 +1,6 @@
-%unction [Graph_new,Arc_new,R_new,Repu_new,TP_new] = RemoveNode(Graph,Arc,R,Repu,TP,TH)
+%function [Graph_new,Arc_new,R_new,Repu_new,TP_new] = RemoveNode(Graph,Arc,R,Repu,TP,TH)
 function [Graph_new,Arc_new,R_new] = RemoveNode(Graph,Arc,R,TH)
-
-    %This function aims to remove those node with little R(R<TH)
+%This function aims to remove those node with little R(R<TH)
 %TH is the THRESHOLD
 Graph_new=Graph;
 Arc_new=Arc;
@@ -9,62 +8,65 @@ R_new=R;
 %Repu_new=Repu;
 %TP_new=TP;
 count=1;
-count_new=1
-for i = 1:length(R)
-    for j = 1:length(R{i})
-        if R{i}(j)<TH
+count_new=1;
+
+% delete
+i = 1;
+j = 1;
+while i <= length(R_new)
+    while j <= length(R_new{i})
+        if R_new{i}(j)<TH
             R_new{i}(j)=[];
+            temp = Graph_new{i}(j);
             Graph_new{i}(j)=[];
-            Graph_new{i}(j)=count_new;
- %           Repu_new(:,count)=66666;
-  %          TP_new(:,count)=77777;
+%           Repu_new(:,count)=66666;
+%           TP_new(:,count)=77777;
             if i==1
-                [remove_Arc_row,~]=find(Arc{i}==Graph{i}(j));
+                [remove_Arc_row,~]=find(Arc_new{i}==temp);
                 Arc_new{i}(remove_Arc_row,:)=[];
-                [change_node_row,~]=find(Arc_new{i}==count);
-                Arc_new{i}(change_node_row,1)=count_new;
-            elseif i==length(R)
-                [remove_Arc_row,~]=find(Arc{i-1}==Graph{i}(j));
+            elseif i==length(R_new)
+                [remove_Arc_row,~]=find(Arc_new{i-1}==temp);
                 Arc_new{i-1}(remove_Arc_row,:)=[];
-                [change_node_row,~]=find(Arc_new{i-1}==count);
-                Arc_new{i}(change_node_row,2)=count_new;
             else
-                [remove_Arc_row,~]=find(Arc{i}==Graph{i}(j));
+                [remove_Arc_row,~]=find(Arc_new{i}==temp);
                 Arc_new{i}(remove_Arc_row,:)=[]; 
-                [change_node_row,~]=find(Arc_new{i}==count);
-                Arc_new{i}(change_node_row,1)=count_new;
-                [remove_Arc_row,~]=find(Arc{i-1}==Graph{i}(j));
+                [remove_Arc_row,~]=find(Arc_new{i-1}==temp);
                 Arc_new{i-1}(remove_Arc_row,:)=[];
-                [change_node_row,~]=find(Arc_new{i-1}==count);
-                Arc_new{i-1}(change_node_row,2)=count_new;
             end
+            j = j-1;
             count_new=count_new-1; 
         end
+        j = j + 1;
         count=count+1;
         count_new=count_new+1;
     end
+    i = i + 1;
 end
 
-
-% Total_node_num=Graph{8}(length(Graph{8}));
-% New_node_num=Graph_new{8}(length(Graph_new{8}));
-
-
-% Graph_new{1}=[1234]
-
-% count=1
-% for i=1:length(R_new)
-%     for j=1:length(R_new{i})
-%         if R_new{i}(j)==count
-%             Graph_new{i}(j)=count;
-%         else
-
-%         end
-%         Graph_new{i}(j)=count;
-
-
-%     end
-% end
+%recount
+count_new=0;
+for i=1:length(Graph_new)
+    for j=1:length(Graph_new{i})
+        count_new = count_new + 1;
+        if Graph_new{i}(j) == count_new
+        else 
+            temp = Graph_new{i}(j);
+            Graph_new{i}(j) = count_new;
+            if i==1
+                [change_node_row,~]=find(Arc_new{i}==temp);
+                Arc_new{i}(change_node_row,1)=count_new;
+            elseif i==length(R)
+                [change_node_row,~]=find(Arc_new{i-1}==temp);
+                Arc_new{i}(change_node_row,2)=count_new;
+            else
+                [change_node_row,~]=find(Arc_new{i}==temp);
+                Arc_new{i}(change_node_row,1)=count_new;
+                [change_node_row,~]=find(Arc_new{i-1}==temp);
+                Arc_new{i-1}(change_node_row,2)=count_new;
+            end
+        end
+    end
+end
 
 
 
