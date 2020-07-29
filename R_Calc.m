99d1c2cc1fefd3b801f3860a05417c0310d7f4e2
Melody618 <Melody618@sjtu.edu.cn>
2020-07-29 20:28:54 +0800

    R更新只考虑payoff

--- R_Calc.m
+++ R_Calc.m
@@ -11,9 +11,9 @@ function R_new = R_calc(Graph,Arc,R,V_list,alpha,a,b,c,R_sigma,gamma,payoff_one_
     g = R;
     for i = 1:length(R)
         for j = 1:length(R{i})
-            temp = a*V_list(Graph{i}(j))+b*calc_parents_V(Graph,Arc,V_list,i,j)+c*calc_childs_V(Graph,Arc,V_list,i,j); 
-            temp = exp(-temp);
-            temp = temp + payoff_one_turn(Graph{i}(j))/ max(payoff_one_turn);
+            %temp = a*V_list(Graph{i}(j))+b*calc_parents_V(Graph,Arc,V_list,i,j)+c*calc_childs_V(Graph,Arc,V_list,i,j); 
+            %temp = exp(-temp);
+            temp = payoff_one_turn(Graph{i}(j))/ max(payoff_one_turn);
             g{i}(j) = gamma*normrnd(temp, 1/R{i}(j)*R_sigma);%
             R_new{i}(j) = max(0.001, (1-alpha)*R{i}(j)+alpha*g{i}(j));
         end
